68124a1bf7b586eef521b25e2abe8b75f4b8547a
whs <hswongac@gmail.com>
2024-12-26 20:18:43 +0800

    Test

---
+++ test.py
@@ -0,0 +1,21 @@
+import os
+from openai import AzureOpenAI
+from dotenv import load_dotenv
+load_dotenv()
+
+client = AzureOpenAI(
+  azure_endpoint = os.getenv("AZURE_OPENAI_ENDPOINT"), 
+  api_key=os.getenv("AZURE_OPENAI_KEY"),  
+  api_version="2024-02-15-preview"
+)
+
+model = os.getenv("AZURE_OPENAI_DEPLOYMENT_NAME")
+
+text_prompt = "Should oxford commas always be used?"
+
+response = client.chat.completions.create(
+  model=model,
+  messages = [{"role":"system", "content":"You are a helpful assistant."},
+               {"role":"user","content":text_prompt},])
+
+print(response.choices[0].message.content)
